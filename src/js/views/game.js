681e4f32cd93d93abafc55140b66faea3c81a044
shebson <stephen@overhead.fm>
2015-04-10 13:58:06 -0400

    Refactor keyboard navigation into delegate view

--- src/js/views/game.js
+++ src/js/views/game.js
@@ -4,15 +4,13 @@ var BaseView = require('./base'),
     GameView = Object.create(BaseView),
     RowView = require('./row'),
     Board = require('../models/board'),
-    WinnerView = require('./winner');
+    WinnerView = require('./winner'),
+    KeyboardNavigator = require('./keyboard-navigator');
 
 GameView.initialize({
     el: "#content",
     template: require('../templates/game.hbs'),
     board: Object.create(Board),
-    events: {
-        "keydown": "handleKeyNav"
-    },
     boardEvents: {
         "invalidRow": "highlightInvalidRow",
         "invalidColumn": "highlightInvalidColumn",
@@ -37,6 +35,11 @@ GameView.initialize({
         $(this.el).html(innerHtml);
         this.board.generate();
         this.renderRows();
+        this.keyboardNavigator = Object.create(KeyboardNavigator);
+        this.keyboardNavigator.initialize({
+            el: "#content",
+            board: this.board
+        });
     },
     renderRows: function () {
         "use strict";
@@ -50,70 +53,6 @@ GameView.initialize({
                 cells: this.board.startingState.rows[i]
             });
             row.render();
-        }
-    },
-    handleKeyNav: function (event) {
-        "use strict";
-        var cell = $(event.target),
-            row = cell.data('row'),
-            column = cell.data('column');
-        switch (event.keyCode) {
-            case 37: // Left
-                this.focusLeft(row, column);
-                break;
-            case 38: // Up
-                this.focusUp(row, column);
-                break;
-            case 39: // Right
-                this.focusRight(row, column);
-                break;
-            case 40: // Down
-                this.focusDown(row, column);
-                break;
-        }
-    },
-    focusIfEditableCell: function (row, column) {
-        var el = $('#game .cell.editable.row-' + row + '.column-' + column);
-        if (el.length > 0) {
-            el.focus().select();
-            return true;
-        }
-        return false;
-    },
-    focusLeft: function (row, column) {
-        column = column - 1;
-        if (this.focusIfEditableCell(row, column)) {
-            return;
-        }
-        if (column > 0) {
-            this.focusLeft(row, column);
-        }
-    },
-    focusRight: function (row, column) {
-        column = column + 1;
-        if (this.focusIfEditableCell(row, column)) {
-            return;
-        }
-        if (column < this.board.currentState.rows[0].length) {
-            this.focusRight(row, column);
-        }
-    },
-    focusUp: function (row, column) {
-        row = row - 1;
-        if (this.focusIfEditableCell(row, column)) {
-            return;
-        }
-        if (row > 0) {
-            this.focusUp(row, column);
-        }
-    },
-    focusDown: function (row, column) {
-        row = row + 1;
-        if (this.focusIfEditableCell(row, column)) {
-            return;
-        }
-        if (row < this.board.currentState.rows.length) {
-            this.focusDown(row, column);
         }
     },
     highlightInvalidRow: function (event, row) {
---
+++ src/js/views/keyboard-navigator.js
@@ -0,0 +1,76 @@
+var BaseView = require('./base'),
+    $ = require('jquery'),
+    _ = require('underscore'),
+    KeyboardNavigator = Object.create(BaseView);
+
+KeyboardNavigator.initialize({
+    events: {
+        "keydown": "handleKeyNav"
+    },
+    handleKeyNav: function (event) {
+        "use strict";
+        var cell = $(event.target),
+            row = cell.data('row'),
+            column = cell.data('column');
+        switch (event.keyCode) {
+            case 37: // Left
+                this.focusLeft(row, column);
+                break;
+            case 38: // Up
+                this.focusUp(row, column);
+                break;
+            case 39: // Right
+                this.focusRight(row, column);
+                break;
+            case 40: // Down
+                this.focusDown(row, column);
+                break;
+        }
+    },
+    focusIfEditableCell: function (row, column) {
+        var el = $('#game .cell.editable.row-' + row + '.column-' + column);
+        if (el.length > 0) {
+            el.focus().select();
+            return true;
+        }
+        return false;
+    },
+    focusLeft: function (row, column) {
+        column = column - 1;
+        if (this.focusIfEditableCell(row, column)) {
+            return;
+        }
+        if (column > 0) {
+            this.focusLeft(row, column);
+        }
+    },
+    focusRight: function (row, column) {
+        column = column + 1;
+        if (this.focusIfEditableCell(row, column)) {
+            return;
+        }
+        if (column < this.board.currentState.rows[0].length) {
+            this.focusRight(row, column);
+        }
+    },
+    focusUp: function (row, column) {
+        row = row - 1;
+        if (this.focusIfEditableCell(row, column)) {
+            return;
+        }
+        if (row > 0) {
+            this.focusUp(row, column);
+        }
+    },
+    focusDown: function (row, column) {
+        row = row + 1;
+        if (this.focusIfEditableCell(row, column)) {
+            return;
+        }
+        if (row < this.board.currentState.rows.length) {
+            this.focusDown(row, column);
+        }
+    }
+});
+
+module.exports = KeyboardNavigator;
